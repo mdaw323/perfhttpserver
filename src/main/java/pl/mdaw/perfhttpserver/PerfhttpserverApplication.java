90b4433ba452103203f3d5dfcfba036cd11f9b7d
mdaw <mdaw@protonmail.com>
2018-12-11 22:02:39 +0100

    Add webflux/netty simple server for load test

---
+++ src/main/java/pl/mdaw/perfhttpserver/PerfhttpserverApplication.java
@@ -0,0 +1,12 @@
+package pl.mdaw.perfhttpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfhttpserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfhttpserverApplication.class, args);
+	}
+}
